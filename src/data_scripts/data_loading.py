fcd3298cda3e8dbd94c5e174d9ed5c0ed5e1ec83
Luca-DTU <s220647@student.dtu.dk>
2024-04-11 14:13:13 +0200

    add prescaling to rans

--- src/data_scripts/data_loading.py
+++ src/data_scripts/data_loading.py
@@ -43,12 +43,14 @@ class DataExtractor():
         data = test_samples[index]
         data["x"] = data["x"].unsqueeze(0)
         data["y"] = data["y"].unsqueeze(0)
-        data_processor.train = False
+        data_processor.train = False 
+        # data_processor.eval()
         # forward pass
         data = data_processor.preprocess(data, batched=True)
         output = model(data["x"])
-        if data_processor.out_normalizer and not data_processor.train:
-            output = data_processor.out_normalizer.inverse_transform(output)
+        # if data_processor.out_normalizer and not data_processor.train:
+        #     output = data_processor.out_normalizer.inverse_transform(output)
+        output, data = data_processor.postprocess(output, data)
         y = data["y"]
         # detach and convert to numpy
         output = output.detach().cpu().numpy()
@@ -65,13 +67,14 @@ class DataExtractor():
 
 
     def evaluate_all(self,test_loader, model,data_processor,losses):
-        loss_avg = 0
+        total_loss = {name: 0 for name in losses.keys()}
         for batch in test_loader:
             data_processor.train = False
             batch = data_processor.preprocess(batch, batched=True)
             output = model(batch["x"])
-            if data_processor.out_normalizer and not data_processor.train:
-                output = data_processor.out_normalizer.inverse_transform(output)
+            # if data_processor.out_normalizer and not data_processor.train:
+            #     output = data_processor.out_normalizer.inverse_transform(output)
+            output, batch = data_processor.postprocess(output, batch)
             y = batch["y"]
             # detach and convert to numpy
             output = output.detach().cpu().numpy()
@@ -83,8 +86,9 @@ class DataExtractor():
             for name, loss_fn in losses.items():
                 loss[name] = loss_fn(output, y).item()
                 log.info(f"Test loss {name}: {loss[name]}")
-            loss_avg += sum(loss.values())
-        return loss_avg
+                total_loss[name] += loss[name]
+        log.info(f"Test loss: {total_loss}")
+        return sum(total_loss.values())
 class rans(DataExtractor):    
     def extract_sample(self,dataset, layout, wd, inputs, outputs):
         x = [dataset[inp].interp(type=layout, wd=wd).data for inp in inputs]
@@ -128,6 +132,41 @@ class rans(DataExtractor):
             y_train = torch.tensor(y_train).float()
             return x_train, y_train
     
+
+class rans_prescaled_independently(rans):
+    # can this be achieved with super
+    def extract_sample(self,dataset, layout, wd, inputs, outputs):
+        x, y = super().extract_sample(dataset, layout, wd, inputs, outputs)
+        # min max scale y over the last two dimensions to allow for multivariate output
+        y = (y-np.min(y, axis = (-2,-1), keepdims = True))/(np.max(y, axis = (-2,-1), keepdims = True)-np.min(y, axis = (-2,-1), keepdims = True))
+        return x, y
+
+class rans_prescaled_on_resolution(rans):
+    def extract(self,turbines_per_side = 4, 
+                    horizontal_grid_spacing = [0.5,1.0,2.0,4.0],
+                    layout_type = np.arange(0,50), 
+                    inflow_wind_direction = [315.0],
+                    path = 'data/RANS/',
+                    inputs = ["A_AWF"],
+                    outputs = ['U', 'V', 'W', 'P', 'muT', 'tke', 'epsilon']):
+        x,y = super().extract(turbines_per_side = turbines_per_side,
+                    horizontal_grid_spacing = horizontal_grid_spacing,
+                    layout_type = layout_type,
+                    inflow_wind_direction = inflow_wind_direction,
+                    path = path,
+                    inputs = inputs,
+                    outputs = outputs)
+        min_max_scale = lambda x: (x-np.min(x, axis = (0,2,3), keepdims = True))/(np.max(x, axis = (0,2,3), keepdims = True)-np.min(x, axis = (0,2,3), keepdims = True))
+        if len(horizontal_grid_spacing) > 1:
+            y = [torch.tensor(min_max_scale(y_.numpy())).float() for y_ in y]
+        else:
+            y = torch.tensor(min_max_scale(y.numpy())).float()
+        return x, y
+    
+            
+
+        
+    
 class synthetic_data(DataExtractor):
     def extract(self,resolution = 32,path = "data/simulated_data/synth_const_dil.pkl", multivariate = True, reduce = None):
         with open(path, "rb") as f:
@@ -163,3 +202,43 @@ class synthetic_data(DataExtractor):
                 x_list.append(x_train)
                 y_list.append(y_train)
             return x_list, y_list
+
+if __name__ == "__main__":
+    # extract rans data at different resolutions and plot
+    # extractor = rans_prescaled_independently()
+    extractor = rans_prescaled_on_resolution()
+    x,y = extractor.extract(horizontal_grid_spacing = [0.5,1.0,2.0,4.0],
+                          inflow_wind_direction = [315.0], outputs = ['U'])
+    for sample in np.random.randint(0,50,4):
+        fig, ax = plt.subplots(2,4,figsize=(20,10))
+        # plot with colorbar for each axis
+        for i in range(4):
+            x_ = x[i][sample,0]
+            y_ = y[i][sample,0]
+            # y_ = (y_-torch.min(y_))/(torch.max(y_)-torch.min(y_))
+
+            im = ax[0,i].imshow(x_)
+            fig.colorbar(im, ax=ax[0,i], orientation='horizontal')
+            im = ax[1,i].imshow(y_)
+            fig.colorbar(im, ax=ax[1,i], orientation='horizontal')
+        plt.tight_layout()
+        # plt.savefig(f"example_{sample}.png")
+        plt.show()
+
+        # fig, ax = plt.subplots(2,4,figsize=(20,10))
+        # # plot with colorbar for each axis
+        # for i in range(4):
+        #     x_ = x[i][sample,0]
+        #     y_ = y[i][sample,0]
+        #     y_ = (y_-torch.min(y_))/(torch.max(y_)-torch.min(y_))
+
+        #     im = ax[0,i].imshow(x_)
+        #     fig.colorbar(im, ax=ax[0,i], orientation='horizontal')
+        #     im = ax[1,i].imshow(y_)
+        #     fig.colorbar(im, ax=ax[1,i], orientation='horizontal')
+        # plt.tight_layout()
+        # plt.savefig(f"example_min_max{sample}.png")
+        # plt.show()
+    pass
+
+
