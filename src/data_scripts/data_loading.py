4fcbb8f1fc9cb7ecb93d574cb4a715c8534ce506
Luca-DTU <s220647@student.dtu.dk>
2024-04-12 15:00:45 +0200

    add data_sources changes for rans

--- src/data_scripts/data_loading.py
+++ src/data_scripts/data_loading.py
@@ -6,6 +6,7 @@ from matplotlib import pyplot as plt
 import logging
 import hydra
 from tqdm import tqdm
+
 log = logging.getLogger(__name__)
 
 class DataExtractor():
@@ -44,12 +45,8 @@ class DataExtractor():
         data["x"] = data["x"].unsqueeze(0)
         data["y"] = data["y"].unsqueeze(0)
         data_processor.train = False 
-        # data_processor.eval()
-        # forward pass
         data = data_processor.preprocess(data, batched=True)
         output = model(data["x"])
-        # if data_processor.out_normalizer and not data_processor.train:
-        #     output = data_processor.out_normalizer.inverse_transform(output)
         output, data = data_processor.postprocess(output, data)
         y = data["y"]
         # detach and convert to numpy
@@ -72,8 +69,6 @@ class DataExtractor():
             data_processor.train = False
             batch = data_processor.preprocess(batch, batched=True)
             output = model(batch["x"])
-            # if data_processor.out_normalizer and not data_processor.train:
-            #     output = data_processor.out_normalizer.inverse_transform(output)
             output, batch = data_processor.postprocess(output, batch)
             y = batch["y"]
             # detach and convert to numpy
@@ -141,7 +136,20 @@ class rans_prescaled_independently(rans):
         y = (y-np.min(y, axis = (-2,-1), keepdims = True))/(np.max(y, axis = (-2,-1), keepdims = True)-np.min(y, axis = (-2,-1), keepdims = True))
         return x, y
 
+
+
 class rans_prescaled_on_resolution(rans):
+    def __init__(self):
+        super().__init__()
+        self.min = {}
+        self.max = {}
+    def min_max_scale(self,x,horizontal_grid_spacing = None):
+        min = np.min(x, axis = (0,2,3), keepdims = True)
+        max = np.max(x, axis = (0,2,3), keepdims = True)
+        self.min[horizontal_grid_spacing] = min.flatten()
+        self.max[horizontal_grid_spacing] = max.flatten()
+        return (x-min)/(max-min)
+
     def extract(self,turbines_per_side = 4, 
                     horizontal_grid_spacing = [0.5,1.0,2.0,4.0],
                     layout_type = np.arange(0,50), 
@@ -156,11 +164,11 @@ class rans_prescaled_on_resolution(rans):
                     path = path,
                     inputs = inputs,
                     outputs = outputs)
-        min_max_scale = lambda x: (x-np.min(x, axis = (0,2,3), keepdims = True))/(np.max(x, axis = (0,2,3), keepdims = True)-np.min(x, axis = (0,2,3), keepdims = True))
+        # min_max_scale = lambda x: (x-np.min(x, axis = (0,2,3), keepdims = True))/(np.max(x, axis = (0,2,3), keepdims = True)-np.min(x, axis = (0,2,3), keepdims = True))
         if len(horizontal_grid_spacing) > 1:
-            y = [torch.tensor(min_max_scale(y_.numpy())).float() for y_ in y]
+            y = [torch.tensor(self.min_max_scale(y_.numpy(),spacing)).float() for spacing,y_ in zip(horizontal_grid_spacing,y)]
         else:
-            y = torch.tensor(min_max_scale(y.numpy())).float()
+            y = torch.tensor(self.min_max_scale(y.numpy())).float()
         return x, y
     
             
@@ -203,42 +211,87 @@ class synthetic_data(DataExtractor):
                 y_list.append(y_train)
             return x_list, y_list
 
-if __name__ == "__main__":
-    # extract rans data at different resolutions and plot
-    # extractor = rans_prescaled_independently()
-    extractor = rans_prescaled_on_resolution()
+def investigate_ranges():
+    # investigate the ranges of the different datasets
+    # note: this is not in the requirements.txt
+    from sklearn.linear_model import LinearRegression
+    extractor = rans()
+    inflow_wind_direction = [270.0, 273.0, 276.0, 279.0, 282.0, 285.0, 288.0, 291.0, 294.0, 297.0,
+                            300.0, 303.0, 306.0, 309.0, 312.0, 315.0]
     x,y = extractor.extract(horizontal_grid_spacing = [0.5,1.0,2.0,4.0],
-                          inflow_wind_direction = [315.0], outputs = ['U'])
-    for sample in np.random.randint(0,50,4):
-        fig, ax = plt.subplots(2,4,figsize=(20,10))
-        # plot with colorbar for each axis
-        for i in range(4):
-            x_ = x[i][sample,0]
-            y_ = y[i][sample,0]
-            # y_ = (y_-torch.min(y_))/(torch.max(y_)-torch.min(y_))
+                          inflow_wind_direction = inflow_wind_direction, outputs = ['U'])
+    # pick a sample and plot
+    sample = 27
+    fig, ax = plt.subplots(2,4,figsize=(10,5))
+    # plot with colorbar for each axis
+    for i in range(4):
+        x_ = x[i][sample,0]
+        y_ = y[i][sample,0]
+        im = ax[0,i].imshow(x_)
+        fig.colorbar(im, ax=ax[0,i], orientation='vertical')
+        im = ax[1,i].imshow(y_)
+        fig.colorbar(im, ax=ax[1,i], orientation='vertical')
+        # remove ticks
+        for a in ax[:,i]:
+            a.set_xticks([])
+            a.set_yticks([])
+    plt.tight_layout()
+    plt.savefig(f"rans_resolution_comparison.png")
+    plt.show()
+    # show linear regression
+    x_res = np.array([0.5,1.0,2.0,4.0]).reshape(-1,1)
+    y_res = [np.min(y_.numpy()) for y_ in y]
+    reg = LinearRegression().fit(x_res, y_res)
+    print(reg.coef_, reg.intercept_)
+    from sklearn.metrics import r2_score
+    r2 = round(r2_score(y_res, reg.predict(x_res)),3)
+    plt.figure(figsize=(5,5))
+    plt.scatter(x_res, y_res)
+    plt.plot(x_res, reg.predict(x_res))
+    plt.xlabel("Horizontal grid spacing")
+    plt.ylabel("Min U")
+    # add text box in top-right corner with r2
+    plt.text(4.12, 0.732, f"$R^2$: {r2}",
+             verticalalignment='top', horizontalalignment='right',
+             bbox=dict(facecolor='white', alpha=0.5))
+    plt.tight_layout()
+    plt.savefig("min_U_regression.png")
+    plt.show()
 
-            im = ax[0,i].imshow(x_)
-            fig.colorbar(im, ax=ax[0,i], orientation='horizontal')
-            im = ax[1,i].imshow(y_)
-            fig.colorbar(im, ax=ax[1,i], orientation='horizontal')
-        plt.tight_layout()
-        # plt.savefig(f"example_{sample}.png")
-        plt.show()
-
-        # fig, ax = plt.subplots(2,4,figsize=(20,10))
-        # # plot with colorbar for each axis
-        # for i in range(4):
-        #     x_ = x[i][sample,0]
-        #     y_ = y[i][sample,0]
-        #     y_ = (y_-torch.min(y_))/(torch.max(y_)-torch.min(y_))
-
-        #     im = ax[0,i].imshow(x_)
-        #     fig.colorbar(im, ax=ax[0,i], orientation='horizontal')
-        #     im = ax[1,i].imshow(y_)
-        #     fig.colorbar(im, ax=ax[1,i], orientation='horizontal')
-        # plt.tight_layout()
-        # plt.savefig(f"example_min_max{sample}.png")
-        # plt.show()
-    pass
+class rans_super_resolution(rans):
+    def extract_single_spacing(self,spacing):
+        x_list = []
+        databasename = 'awf_database_%gcD.nc' % spacing
+        databasename = self.path + databasename
+        dataset = xarray.open_dataset(databasename)
+        for layout in tqdm(self.layout_type):
+            for wd in self.inflow_wind_direction:
+                _, y = self.extract_sample(dataset, layout, wd, self.inputs, self.outputs)
+                x_list.append(y)
+        # close dataset
+        dataset.close()
+        train_data = np.stack(x_list,0)
+        train_data = torch.tensor(train_data).float()
+        return train_data
+    def extract(self, 
+                input_spacing = 0.5,
+                output_spacing = 1.0,
+                layout_type = np.arange(0,50),
+                inflow_wind_direction = [315.0],
+                path = 'data/RANS/',
+                inputs = ["A_AWF"],
+                outputs = ['U', 'V', 'W', 'P', 'muT', 'tke', 'epsilon'],
+                **kwargs):
+        self.path = path
+        self.inputs = inputs
+        self.outputs = outputs
+        self.layout_type = layout_type
+        self.inflow_wind_direction = inflow_wind_direction
+        x_train = self.extract_single_spacing(input_spacing)
+        y_train = self.extract_single_spacing(output_spacing)
+        return x_train, y_train
+        
+if __name__ == "__main__":
+    investigate_ranges()
 
 
