165393fddc77be413631b562ad033e296cc411d4
Luca-DTU <s220647@student.dtu.dk>
2024-05-10 16:16:07 +0200

    evaluate layout

--- scripts/evaluate_angle_spacing.py
+++ scripts/evaluate_angle_spacing.py
@@ -47,57 +47,63 @@ def evaluate_angle_spacing(model_folder = "multirun/2024-04-27/15-16-35/8"):
     loss_dict = {}
     for direction in all_directions:
         config.data_source.test_args.inflow_wind_direction = [direction]
-        test_args = config.data_source.train_args
-        test_args.update(config.data_source.test_args)
-        x_test,y_test = data_source.extract(**test_args)
-        # load model
-        if "model" not in locals():
-            if config.data_format.positional_encoding:
-                input_channels = x_test.shape[1]+2
-            else:
-                input_channels = x_test.shape[1]
-            out_channels = y_test.shape[1]
-            if "non_linearity" in config.TFNO:
-                non_linearity = getattr(torch.nn.functional, config.TFNO.non_linearity) 
-                kwargs = OmegaConf.to_container(config.TFNO)
-                kwargs["non_linearity"] = non_linearity
-            else:
-                kwargs = OmegaConf.to_container(config.TFNO)
-            if config.get("super_resolution"):
-                model = SuperResolutionTFNO(**kwargs, in_channels=input_channels, 
-                                            out_channels=out_channels, out_size=y_test.shape[2:])
-            else:
-                model = TFNO(**kwargs, in_channels=input_channels, out_channels=out_channels)
-            model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
-            model.eval()
-            # load data processor
-            with open(data_processor_path, "rb") as f:
-                data_processor = pickle.load(f)
-            if isinstance(data_processor, list):
-                data_processor = data_processor[-1]
-            data_processor = data_processor.to("cpu")
-            # eval losses
-            l2loss = LpLoss(d=2, p=2,reduce_dims=[0,1]) # d=2 is the spatial dimension, p=2 is the L2 norm, reduce_dims=[0,1] means that the loss is averaged over the spatial dimensions 0 and 1
-            h1loss = H1Loss(d=2,reduce_dims=[0,1]) # d=2 is the spatial dimension, reduce_dims=[0,1] means that the loss is averaged over the spatial dimensions 0 and 1
-            eval_losses = {'l2': l2loss, 'h1': h1loss}
-            # test_batch_size = config.train.test_batch_size
-            test_batch_size = 4
-        # dummy test loader
-        test_db = TensorDataset(
-            x_test,
-            y_test,
-        )
-        test_loader = torch.utils.data.DataLoader(
-            test_db,
-            batch_size=test_batch_size,
-            shuffle=False,
-            num_workers=0,
-            pin_memory=True,
-            persistent_workers=False,
-        )
-        test_loss = data_source.evaluate(test_loader,model,data_processor,losses=eval_losses,save = False, plot=False, output_names = ["U"])
-        loss_dict[direction] = test_loss
-    loss_df = pd.Series(loss_dict)
+        layout_loss_dict = {}
+        for layout_type in range(50):
+            config.data_source.test_args.layout_type = [layout_type]
+        
+            test_args = config.data_source.train_args
+            test_args.update(config.data_source.test_args)
+            x_test,y_test = data_source.extract(**test_args)
+            # load model
+            if "model" not in locals():
+                if config.data_format.positional_encoding:
+                    input_channels = x_test.shape[1]+2
+                else:
+                    input_channels = x_test.shape[1]
+                out_channels = y_test.shape[1]
+                if "non_linearity" in config.TFNO:
+                    non_linearity = getattr(torch.nn.functional, config.TFNO.non_linearity) 
+                    kwargs = OmegaConf.to_container(config.TFNO)
+                    kwargs["non_linearity"] = non_linearity
+                else:
+                    kwargs = OmegaConf.to_container(config.TFNO)
+                if config.get("super_resolution"):
+                    model = SuperResolutionTFNO(**kwargs, in_channels=input_channels, 
+                                                out_channels=out_channels, out_size=y_test.shape[2:])
+                else:
+                    model = TFNO(**kwargs, in_channels=input_channels, out_channels=out_channels)
+                model.load_state_dict(torch.load(model_path, map_location=torch.device('cpu')))
+                model.eval()
+                # load data processor
+                with open(data_processor_path, "rb") as f:
+                    data_processor = pickle.load(f)
+                if isinstance(data_processor, list):
+                    data_processor = data_processor[-1]
+                data_processor = data_processor.to("cpu")
+                # eval losses
+                l2loss = LpLoss(d=2, p=2,reduce_dims=[0,1]) # d=2 is the spatial dimension, p=2 is the L2 norm, reduce_dims=[0,1] means that the loss is averaged over the spatial dimensions 0 and 1
+                h1loss = H1Loss(d=2,reduce_dims=[0,1]) # d=2 is the spatial dimension, reduce_dims=[0,1] means that the loss is averaged over the spatial dimensions 0 and 1
+                eval_losses = {'l2': l2loss, 'h1': h1loss}
+                # test_batch_size = config.train.test_batch_size
+                test_batch_size = 1
+            # dummy test loader
+            test_db = TensorDataset(
+                x_test,
+                y_test,
+            )
+            test_loader = torch.utils.data.DataLoader(
+                test_db,
+                batch_size=test_batch_size,
+                shuffle=False,
+                num_workers=0,
+                pin_memory=True,
+                persistent_workers=False,
+            )
+            test_loss = data_source.evaluate(test_loader,model,data_processor,losses=eval_losses,save = False, plot=False, output_names = ["U"])
+            layout_loss_dict[layout_type] = test_loss
+        loss_dict[direction] = layout_loss_dict
+    loss_df = pd.DataFrame(loss_dict)
+    loss_df.index.name = "layout_type"
     return loss_df
 
 if __name__ == "__main__":  
@@ -106,4 +112,16 @@ if __name__ == "__main__":
     # store the loss values in the model folder
     loss_df.to_csv(os.path.join(model_folder,"wind_direction_error.csv"))
     # loss_df = pd.read_csv(os.path.join(model_folder,"wind_direction_error.csv"),index_col=0)
-    plot_wind_direction_error(loss_df)
+    wind_dir_error = loss_df.mean(axis=0)
+    plot_wind_direction_error(wind_dir_error)
+    layout_error = loss_df.mean(axis=1)
+
+    spacing_error = layout_error.groupby(layout_error.index // 10).mean()
+    spacing_error.plot(kind='bar')
+    plt.xlabel("Spacing")
+    plt.ylabel("Mean L2 test loss")
+    # Mean over each unit value
+    shape_error = layout_error.groupby(layout_error.index % 10).mean()
+    shape_error.plot(kind='bar')
+    plt.xlabel("Shape")
+    plt.ylabel("Mean L2 test loss")
